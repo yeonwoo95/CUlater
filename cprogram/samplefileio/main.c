c1fdc1c5933beb4add161ac0f65f4fe2e46ffaae
0916dhkim <0916dhkim@gmail.com>
2014-12-26 14:54:10 -0600

    File I/O Program
    Everything is compiled
    just run it

---
+++ cprogram/samplefileio/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	FILE* fileText;			//file handling object
+	char filename[1024];	//string to save filename
+	char lineRead[1024];	//string to save each line of the file
+	int i;					//line counter
+
+	printf("use filename \"this.txt\" to test\n");
+	printf("Input filename: ");
+	scanf("%s",filename);	//read filename from user
+
+	printf("Opening file: %s\n\n",filename);
+	fileText = fopen(filename,"r");		//open file and handle with fileText object
+
+	if(fileText==NULL){					//if file could not be opened, print error on the screen
+		printf("Error opening the file\n");
+		return 0;
+	}
+
+	i = 0;					//set i to 0
+	while( fgets(lineRead,1024,fileText) ){		//if line is not NULL, print the line and loop to next line
+		i++;				//count lines
+		printf("%d\t%s",i,lineRead);
+	}
+
+	printf("\n\n# of lines: %d\n",i);
+
+	fclose(fileText);		//close the file before the program terminates
+
+	return 0;
+}
+
+/* save your textfile to CUlater\cprogram\samplefileio\ to make sure the program works properly */
