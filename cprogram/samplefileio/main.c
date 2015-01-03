895f97a6301f8961a9aba065b4f50cbf75f49f43
0916dhkim <0916dhkim@gmail.com>
2015-01-03 11:10:48 -0600

    Prettier Print
    Fixed how the lines print on the screen

--- cprogram/samplefileio/main.c
+++ cprogram/samplefileio/main.c
@@ -23,11 +23,17 @@ int main(){
 	while( fgets(lineRead,1024,fileText) ){		//if line is not NULL, print the line and loop to next line
 		i++;				//count lines
 		printf("%d\t%s",i,lineRead);
-        printf("Ascii value of the file: ");
+        printf("\tAscii:\t");
 
 		int j=0;
-		while (lineRead[j])
-			printf("%d ",lineRead[j++]);
+		while (lineRead[j]){
+			printf("%4d ",lineRead[j++]);
+			if(j%5==0&&j!=0){
+				printf("\n\t\t");
+			}
+		}
+
+		printf("\n");
 	}
 
 	printf("\n\n# of lines: %d\n",i);
